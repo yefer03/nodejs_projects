59dd22fce7d3b5a01aee5a12656d855a6c05107d
yefer03 <99223918+yefer03@users.noreply.github.com>
2022-11-05 19:09:03 -0500

    Add files via upload

---
+++ restserver/routes/usuarios.js
@@ -0,0 +1,53 @@
+
+const { Router } = require('express');
+const { check } = require('express-validator');
+
+const {
+    validarCampos,
+    validarJWT,
+    esAdminRole,
+    tieneRole
+} = require('../middlewares');
+
+
+const { esRoleValido, emailExiste, existeUsuarioPorId } = require('../helpers/db-validators');
+
+const { usuariosGet,
+        usuariosPut,
+        usuariosPost,
+        usuariosDelete,
+        usuariosPatch } = require('../controllers/usuarios');
+
+const router = Router();
+
+
+router.get('/', usuariosGet );
+
+router.put('/:id',[
+    check('id', 'No es un ID válido').isMongoId(),
+    check('id').custom( existeUsuarioPorId ),
+    check('rol').custom( esRoleValido ), 
+    validarCampos
+],usuariosPut );
+
+router.post('/',[
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'El password debe de ser más de 6 letras').isLength({ min: 6 }),
+    check('correo', 'El correo no es válido').isEmail(),
+    check('correo').custom( emailExiste ),
+    check('rol').custom( esRoleValido ), 
+    validarCampos
+], usuariosPost );
+
+router.delete('/:id',[
+    validarJWT,
+    tieneRole('ADMIN_ROLE', 'VENTAR_ROLE','OTRO_ROLE'),
+    check('id', 'No es un ID válido').isMongoId(),
+    check('id').custom( existeUsuarioPorId ),
+    validarCampos
+],usuariosDelete );
+
+router.patch('/', usuariosPatch );
+
+
+module.exports = router;
